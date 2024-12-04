4dc7048fb24f6ea3a8fd37dda4031aeb1f9caa80
Daniel Szalay <sza.dan.uj@gmail.com>
2024-12-04 13:54:01 +0100

    day 2 part 2 solution

--- src/day02/Main.java
+++ src/day02/Main.java
@@ -8,22 +8,28 @@ import java.util.Arrays;
 public class Main {
     public static void main(String[] args) {
         String fileName = "src/day02/input.txt";
-        int safeCount = 0;
+        int safeCountStrict = 0;
+        int safeCountRelaxed = 0;
         try(BufferedReader reader = new BufferedReader(new FileReader(fileName))) {
             String line;
             while((line = reader.readLine()) != null) {
                 int[] report = Arrays.stream(line.split(" "))
                         .mapToInt(Integer::parseInt)
                         .toArray();
-                if(isSafeLevel(report)) safeCount++;
+                if(isSafeLevelStrict(report)) safeCountStrict++;
+                if(isSafeLevelRelaxed(report)) safeCountRelaxed++;
+                System.out.println("--------------------------------------------------------");
+                System.out.println((isSafeLevelStrict(report) ? "SAFE IN STRICT MODE " : "UNSAFE IN STRICT MODE ") + Arrays.toString(report));
+                System.out.println((isSafeLevelRelaxed(report) ? "SAFE IN RELAXED MODE " : "UNSAFE IN RELAXED MODE ") + Arrays.toString(report));
             }
-                System.out.println("SAFE LEVELS: " + safeCount);
+            System.out.println("SAFE LEVELS STRICT MODE: " + safeCountStrict);
+            System.out.println("SAFE LEVELS RELAXED MODE: " + safeCountRelaxed);
         } catch (IOException e) {
             System.err.println(e.getMessage());
         }
     }
     
-    private static boolean isSafeLevel(int[] input) {
+    private static boolean isSafeLevelStrict(int[] input) {
         if(unsafeJump(input[0], input[1])) return false;
         boolean isIncreasing = input[0] < input[1];
         for (int i = 2; i < input.length; i++) {
@@ -39,7 +45,28 @@ public class Main {
         return true;
     }
     
+    private static boolean isSafeLevelRelaxed(int[] input) {
+        if(isSafeLevelStrict(input)) return true;
+        for (int i = 0; i < input.length; i++) {
+            if(isSafeLevelStrict(removeByIndex(input, i))) return true;
+        }
+        return false;
+    }
+    
     private static boolean unsafeJump(int a, int b) {
         return Math.abs(a - b) == 0 || Math.abs(a - b) > 3;
+    }
+    
+    public static int[] removeByIndex(int[] array, int indexToRemove) {
+        if (indexToRemove < 0 || indexToRemove >= array.length) {
+            throw new IllegalArgumentException("Index out of bounds.");
+        }
+        int[] result = new int[array.length - 1];
+        for (int i = 0, j = 0; i < array.length; i++) {
+            if (i != indexToRemove) {
+                result[j++] = array[i];
+            }
+        }
+        return result;
     }
 }
