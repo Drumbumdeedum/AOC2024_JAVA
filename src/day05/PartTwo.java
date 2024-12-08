a45b9f423e39fae8ba57b8294410bbcfe602f323
Daniel Szalay <sza.dan.uj@gmail.com>
2024-12-08 11:51:58 +0100

    day 5 part 2 solution

--- src/day05/PartTwo.java
+++ src/day05/PartTwo.java
@@ -5,7 +5,7 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
 
-public class PartOne {
+public class PartTwo {
     public static void main(String[] args) {
         String fileName = "src/day05/input.txt";
         
@@ -14,7 +14,7 @@ public class PartOne {
             int total = 0;
             ArrayList<Pair<String, String>> rules = new ArrayList<>();
             ArrayList<String> updates = new ArrayList<>();
-            ArrayList<String> validUpdates = new ArrayList<>();
+            ArrayList<String> invalidUpdates = new ArrayList<>();
             while((line = reader.readLine()) != null) {
                 if(line.contains("|")) {
                     String[] splitResult = line.split("\\|");
@@ -41,12 +41,27 @@ public class PartOne {
                         }
                     }
                 }
-                if(validUpdate) {
-                    validUpdates.add(update);
+                if(!validUpdate) {
+                    invalidUpdates.add(update);
                 }
             }
-            for(String validUpdate: validUpdates) {
-                String[] updateEntries = validUpdate.split(",");
+            for(String invalidUpdate: invalidUpdates) {
+                String[] updateEntries = invalidUpdate.split(",");
+                for (int i = 0; i < updateEntries.length; i++) {
+                    for (int j = 0; j < updateEntries.length; j++) {
+                        if(i != j) {
+                            for(Pair<String, String> rule: rules) {
+                                if(rule.getleft().equals(updateEntries[i])) {
+                                    if(rule.getright().equals(updateEntries[j]) && i > j) {
+                                        String temp = updateEntries[j];
+                                        updateEntries[j] = updateEntries[i];
+                                        updateEntries[i] = temp;
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
                 String middleValue = updateEntries[(int) (double) updateEntries.length / 2];
                 total += Integer.parseInt(middleValue);
             }
