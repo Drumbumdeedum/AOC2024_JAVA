a456d896ae251f68c4351c01f542baa58757ab59
Daniel Szalay <sza.dan.uj@gmail.com>
2024-12-14 14:26:14 +0100

    day6 part 2 solution

--- src/day06/altTake/LabMap.java
+++ src/day06/altTake/LabMap.java
@@ -3,12 +3,14 @@ package day06.altTake;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 
 public class LabMap {
+    public static final boolean testMode = false;
     public static void main(String[] args) {
-        String[][] mapGrid = initMap(false);
+        String[][] mapGrid = initMap();
         int guardX = 0;
         int guardY = 0;
         boolean guardFound = false;
@@ -38,16 +40,18 @@ public class LabMap {
         int guardDir = 0;
         int nextX;
         int nextY;
-        HashMap<Integer, Integer> visitedFields = new HashMap<>();
+        HashMap<Integer, ArrayList<Integer>> visitedFields = new HashMap<>();
         while(true) {
-            //printMap(mapGrid, visitedFields);
+            printMap(mapGrid, visitedFields, guardX, guardY, guardDir);
             int visitedField = Integer.parseInt(String.valueOf(guardX).concat(String.valueOf(guardY)));
+            ArrayList<Integer> visitedFrom;
             if (visitedFields.containsKey(visitedField)) {
-                visitedFields.put(visitedField, visitedFields.get(visitedField) + 1);
+                visitedFrom = visitedFields.get(visitedField);
             } else {
-                visitedFields.put(visitedField, 1);
+                visitedFrom = new ArrayList<>();
             }
-            visitedFields.put(visitedField, visitedFields.get(visitedField) + 1);
+            visitedFrom.add(guardDir);
+            visitedFields.put(visitedField, visitedFrom);
             nextX = guardX + directions[guardDir][0];
             nextY = guardY + directions[guardDir][1];
             if(nextX == -1 || nextY == -1 || nextX == mapGrid[0].length || nextY == mapGrid.length) break;
@@ -58,11 +62,11 @@ public class LabMap {
                 guardY = nextY;
             }
         }
-        //printMap(mapGrid, visitedFields);
-        System.out.println(visitedFields.size());
+        printMap(mapGrid, visitedFields, -1, -1, 0);
+        System.out.println("UNIQUE FIELDS VISITED: " + visitedFields.size());
     }
     
-    public static String[][] initMap(boolean testMode) {
+    public static String[][] initMap() {
         String filePath = testMode ? "src/day06/testInput.txt" : "src/day06/input.txt";
         String[][] result = new String[0][];
         try {
@@ -77,17 +81,40 @@ public class LabMap {
         return result;
     }
     
-    public static void printMap(String[][] mapGrid, HashMap<Integer, Integer> visitedFields) {
-        System.out.println();
-        for (int y = 0; y < mapGrid.length; y++) {
-            for (int x = 0; x < mapGrid[y].length; x++) {
-                if(visitedFields.containsKey(Integer.parseInt(String.valueOf(x).concat(String.valueOf(y))))) {
-                    System.out.print("X ");
-                } else {
-                    System.out.print(mapGrid[y][x] + " ");
-                }
-            }
+    public static void printMap(String[][] mapGrid, HashMap<Integer, ArrayList<Integer>> visitedFields, int guardX, int guardY, int guardDir) {
+        if (testMode) {
             System.out.println();
+            for (int y = 0; y < mapGrid.length; y++) {
+                for (int x = 0; x < mapGrid[y].length; x++) {
+                    if(guardX == x && guardY == y) {
+                        System.out.print(getGuardDirString(guardDir));
+                    } else if(visitedFields.containsKey(Integer.parseInt(String.valueOf(x).concat(String.valueOf(y))))) {
+                        ArrayList<Integer> visitedFrom = visitedFields.get(Integer.parseInt(String.valueOf(x).concat(String.valueOf(y))));
+                        if((visitedFrom.contains(0) || visitedFrom.contains(2)) && (visitedFrom.contains(1) || visitedFrom.contains(3))) {
+                            System.out.print("+ ");
+                        } else if(visitedFrom.contains(0) || visitedFrom.contains(2)) {
+                            System.out.print("| ");
+                        }else if(visitedFrom.contains(1) || visitedFrom.contains(3)) {
+                            System.out.print("- ");
+                        } else {
+                            System.out.print("X ");
+                        }
+                    } else {
+                        System.out.print(mapGrid[y][x] + " ");
+                    }
+                }
+                System.out.println();
+            }
         }
+    }
+    
+    public static String getGuardDirString(int guardDir) {
+        return switch(guardDir) {
+            case 0 -> "^ ";
+            case 1 -> "> ";
+            case 2 -> "v ";
+            case 3 -> "< ";
+            default -> "  ";
+        };
     }
 }
